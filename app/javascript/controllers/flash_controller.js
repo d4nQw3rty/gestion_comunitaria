4819969c57d797fca4e7bf1483d650f67dab8afc
Danys Linares <danlinmoy@gmail.com>
2023-07-07 15:22:25 -0400

    Add modal new head

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -5,4 +5,22 @@ export default class extends Controller {
   dismiss() {
     this.element.remove();
   }
+
+  close(e) {
+    // Prevent default action
+    e.preventDefault();
+    // Remove from parent
+    const modal = document.getElementById("modal-edit-heads");
+    const newHead = document.getElementById("new-head")
+    modal.innerHTML = "";
+    newHead.innerHTML ="";
+
+    // Remove the src attribute from the modal
+    modal.removeAttribute("src");
+    newHead.removeAttribute("src");
+
+    // Remove complete attribute
+    modal.removeAttribute("complete");
+    newHead.removeAttribute("complete");
+  }
 }
