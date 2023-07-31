3502a317460da9daa038f8496cbb0912a08b157c
Danys Linares <danlinmoy@gmail.com>
2023-07-31 17:59:20 -0400

    refactor eligible_members

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,19 +1,24 @@
 class ReportsController < ApplicationController
   def eligible_members
+    @user = current_user
     @end_date = params[:end_date] ? Date.parse(params[:end_date]) : Date.today
-    members = Member.eligible_members(@end_date)
-    heads = Head.eligible_heads(@end_date)
-    @members = members + heads
+  
+    # Obtener los miembros y los heads elegibles
+    @members = Member.joins(head: :user).where(users: { id: @user.id }).eligible_members(@end_date)
+    @heads = Head.joins(:user).where(users: { id: @user.id }).eligible_heads(@end_date)
+ 
 
     # Ordenar por fecha de nacimiento (menor -> mayor)
-    @members.sort_by! { |m| m.birthdate }
+    #@members.sort_by! { |m| [m.head&.head_birthdate, m.member_birthdate] }
 
     # Revertir la lista para obtener el orden deseado (mayor -> menor)
-    @members.reverse!
-    # Contar el total de personas
-    @total_people = @members.count
-  end
+   # @members.reverse!
 
+    # Contar el total de personas
+    @count = @heads.size + @members.size
+  end
+  
+  
   def clap
     @user = current_user
     @heads = Head.includes(:user)
