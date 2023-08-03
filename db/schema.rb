fb070ffac872d94dc02fe811fd9054964eb7613b
Danys Linares <danlinmoy@gmail.com>
2023-08-03 12:18:08 -0400

    add cities

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_01_223755) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_02_180252) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.bigint "state_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["state_id"], name: "index_cities_on_state_id"
+  end
 
   create_table "head_social_aids", force: :cascade do |t|
     t.bigint "head_id", null: false
@@ -105,16 +113,20 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_01_223755) do
     t.string "comuna"
     t.string "ubch"
     t.bigint "state_id"
+    t.bigint "city_id"
+    t.index ["city_id"], name: "index_users_on_city_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["state_id"], name: "index_users_on_state_id"
   end
 
+  add_foreign_key "cities", "states"
   add_foreign_key "head_social_aids", "heads"
   add_foreign_key "head_social_aids", "social_aids"
   add_foreign_key "heads", "social_aids"
   add_foreign_key "heads", "users"
   add_foreign_key "members", "heads"
   add_foreign_key "social_aids_heads", "heads"
+  add_foreign_key "users", "cities"
   add_foreign_key "users", "states"
 end
